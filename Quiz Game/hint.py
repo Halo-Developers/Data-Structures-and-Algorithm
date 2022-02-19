b833480f9d32969cf44c3f86b7fde045ddb27cb9
kuyesu <2019bce028@std.must.ac.ug>
2022-02-19 10:12:17 +0000

    quiz hint

---
+++ Quiz Game/hint.py
@@ -0,0 +1,28 @@
+"""Quiz Game using Dictionary"""
+
+"""
+Stpes:
+    1. Create a dictionary containing questions and answers
+    2. Loop through diction
+    3. 
+"""
+
+question_and_answer = {
+    "What is the capital of India?": "New Delhi",
+    "What is the capital of USA?": "Washington",
+    "What is the capital of UK?": "London",
+    "What is the capital of Germany?": "Berlin",
+    "What is the capital of France?": "Paris",
+    "What is the capital of Italy?": "Rome",
+    "What is the capital of Australia?": "Canberra",
+    "What is the capital of Canada?": "Ottawa",
+    "What is the capital of New Zealand?": "Wellington",
+    "What is the capital of South Africa?": "Pretoria",
+    "What is the capital of the Netherlands?": "Amsterdam",
+    "What is the capital of the United Kingdom?": "London",
+    "What is the capital of the United States?": "Washington",
+    "What is the capital of the United Arab Emirates?": "Abu Dhabi",
+    "What is the capital of the United Kingdom?": "London",
+    "What is the capital of the United States?": "Washington",
+}
+
