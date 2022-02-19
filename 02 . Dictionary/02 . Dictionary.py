c75f6dcb5c8975d7a636a686c912a5a5419f2c8f
kuyesu <2019bce028@std.must.ac.ug>
2022-02-19 10:16:29 +0000

    adding item from user input

--- 02 . Dictionary/02 . Dictionary.py
+++ 02 . Dictionary/02 . Dictionary.py
@@ -49,14 +49,16 @@ The methods of dictionary are:
 
 student1["Goeffrey"] = "Goeffrey 23 cpga 4.5 marks 90"
 
-# student_key = input("Enter the key: ")
-# student_name = input("Enter the name of the student: ")
-# student_age = int(input("Enter the age of the student: "))
-# student_marks = int(input("Enter the marks of the student: "))
-# student_cgpa = float(input("Enter the cgpa of the student: "))
 
-# student1[student_key] = f"Name: {student_name} Age: {student_age} CGPA: {student_cgpa} Marks: {student_marks}"
-# # student1 = dict(James="James 23 cpga 4.5 marks 90")
+"""Adding items to dictionary from user input"""
+student_key = input("Enter the key: ")
+student_name = input("Enter the name of the student: ")
+student_age = int(input("Enter the age of the student: "))
+student_marks = int(input("Enter the marks of the student: "))
+student_cgpa = float(input("Enter the cgpa of the student: "))
+
+student1[student_key] = f"Name: {student_name} Age: {student_age} CGPA: {student_cgpa} Marks: {student_marks}"
+# student1 = dict(James="James 23 cpga 4.5 marks 90")
 
 
 print(student1)
